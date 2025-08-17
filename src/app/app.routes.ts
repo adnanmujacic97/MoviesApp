5dc5ad144d410bed659a43799ad422c4b34a48d9
adnanmujacic97 <mujacic.ado@gmail.com>
2025-08-18 01:07:29 +0200

    Mostly figured out routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MoviesComponent } from './movies/movies.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{ path: '', component: HomeComponent }, // Default ruta
+{ path: 'movies', component: MoviesComponent }, // /kontakti prikazuje KontaktiComponent
+{ path: 'about', component: AboutComponent }, // /about prikazuje AboutComponent
+{ path: '**', redirectTo: "", pathMatch: 'full' } // Fallback ruta
+];
+
+// @NgModule({
+// imports: [RouterModule.forRoot(routes)],
+// exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

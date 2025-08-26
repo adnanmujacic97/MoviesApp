eff907323f055cbb0104b643f2b404749767a266
Adnan AM. Mujacic <adnanmujacic@Infonet.intern>
2025-08-26 16:45:53 +0200

    Movies and event binding

--- src/app/movies/movie.model.ts
+++ src/app/movies/movie.model.ts
@@ -1,8 +1,13 @@
 export class Movie {
   constructor(
+    public id: number,
     public title: string,
     public description: string,
     public imageUrl: string,
     public price: number
   ) {}
+
+  printPrice(){
+    return this.price + ' KM'
+  }
 }
